04e3b2f078d6cd387ce1ce14deb2e46d781682b3
Carlos Villamil <carlosvillamilgrisales@gmail.com>
2024-11-15 19:19:41 -0500

    Archivos base

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('actionButton').addEventListener('click', function () {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = '¡Gracias por hacer clic!';
+    messageElement.style.color = '#007BFF';
+    
+});
